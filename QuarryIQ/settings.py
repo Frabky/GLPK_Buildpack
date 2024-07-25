9024b9397003595c469347fcee1565ebb7bcb4c2
François Barbieux <francois.barbieux@sagitta-nova.com>
2024-07-25 19:57:30 +0200

    Set up for Heroku Deployment

--- QuarryIQ/settings.py
+++ QuarryIQ/settings.py
@@ -132,9 +132,7 @@ WSGI_APPLICATION = "QuarryIQ.wsgi.application"
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    )
+    'default': dj_database_url.config(default='postgres://localhost')
 }
 
 
